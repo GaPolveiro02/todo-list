873981cb0faf7d6e07e6f86e407b4a6412116efa
Gabriel Polveiro <gabriel-polveiro@hotmail.com>
2020-07-28 20:49:07 -0300

    create addTodo and renderTodo methods

---
+++ todosNovo.js
@@ -0,0 +1,28 @@
+const listElement = document.querySelector('#app ul');
+const inputElement = document.querySelector('#app input');
+const buttonElement = document.querySelector('#app button');
+
+const todoList = [];
+
+function addTodo() {
+    const todoItem = document.createElement('li');
+    const contentItem = document.createTextNode('da o bumbum');
+    todoItem.appendChild(contentItem); 
+
+    
+
+
+    todoList.push(todoItem);
+    console.log(todoList);
+    renderTodos();
+};
+
+function renderTodos() {
+    console.log('passei aqui');
+    for (const item of todoList) {
+        listElement.appendChild(item);
+    };
+};
+
+
+addTodo();
