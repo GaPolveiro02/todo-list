e5c7382e05b6f22bba44f340a1b6da380f982325
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-07-29 01:55:35 -0300

    removeTodo implements(missing)

--- todosNovo.js
+++ todosNovo.js
@@ -6,7 +6,7 @@ const todoList = [];
 
 function addTodo(e) {
     e.preventDefault();
-    let inputValue = inputElement.value;
+    const inputValue = inputElement.value;
 
     if (inputValue) {
         const a = createTodo(inputValue);
@@ -20,15 +20,31 @@ function addTodo(e) {
 function createTodo(content) {
     const todoItem = document.createElement('li');
     const contentItem = document.createTextNode(content);
+
+    const excludeButton = document.createElement('button');
+    const excludeButtonContent = document.createTextNode('x');
+
+    // excludeButton.setAttribute('onclick', `removeTodo${pos}`);
+
+    excludeButton.style.marginLeft = '10px';
+    excludeButton.appendChild(excludeButtonContent);
+
     todoItem.appendChild(contentItem);
+    todoItem.appendChild(excludeButton);
 
     return todoItem;
 }
 
 function renderTodos() {
     for (const item of todoList) {
+
         listElement.appendChild(item);
     };
 };
+
+function removeTodo(item) {
+    todoList.splice(item, 1);
+    renderTodos();
+}
 
 formElement.addEventListener('submit', (e) => addTodo(e));
